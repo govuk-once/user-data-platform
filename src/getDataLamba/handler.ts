51212dd0e72f345637a5d8153f59af8f73be8b56
Brian Bayes <brian.bayes@digital.cabinet-office.gov.uk>
2025-11-25 15:38:44 +0000

    chore: address typos and use V2 proxy event

--- src/getDataLamba/handler.ts
+++
@@ -1,8 +0,0 @@
-import type { APIGatewayProxyEvent } from 'aws-lambda';
-
-export const handler = async (event: APIGatewayProxyEvent) => {
-  return {
-    status: 200,
-    body: 'Hello GET data',
-  };
-};
---
+++ src/getDataLambda/handler.ts
@@ -0,0 +1,8 @@
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayProxyEventV2) => {
+  return {
+    status: 200,
+    body: 'Hello GET data',
+  };
+};
