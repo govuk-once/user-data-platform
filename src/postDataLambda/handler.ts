94f8d9616bb874c00f7aedff50f6ac5204498d69
phillarmstrong-GDS <phill.armstrong@digital.cabinet-office.gov.uk>
2025-11-26 11:31:51 +0000

    removinh .nx files

--- src/postDataLambda/handler.ts
+++ src/postDataLambda/handler.ts
@@ -1,6 +1,6 @@
-import type { APIGatewayProxyEvent } from 'aws-lambda';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
 
-export const handler = async (event: APIGatewayProxyEvent) => {
+export const handler = async (event: APIGatewayProxyEventV2) => {
   return {
     status: 200,
     body: 'Hello POST data',
