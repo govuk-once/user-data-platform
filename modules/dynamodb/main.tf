1a0e1df84638867c9d8929211c52007235136ba1
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2025-11-26 19:56:11 +0000

    chore: dynamo db infrastructure

---
+++ modules/dynamodb/main.tf
@@ -0,0 +1,25 @@
+resource "aws_dynamodb_table" "this" {
+    name = "${var.table_name}-${var.environment}"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = var.hash_key
+    range_key = var.sort_key
+
+    attribute {
+      name = var.hash_key
+      type = "S"
+    }
+
+    attribute {
+      name = var.sort_key
+      type ="S"
+    }
+
+    server_side_encryption {
+      enabled = var.enable_encription
+      kms_key_arn = var.kms_key_arn
+    }
+
+    point_in_time_recovery {
+      enabled = var.point_in_time_recovery_enabled
+    }
+}
---
+++ modules/dynamodb/variables.tf
@@ -0,0 +1,39 @@
+variable "table_name" {
+    description = "Name of Dynamo Table"
+    type = string
+}
+
+variable "hash_key" {
+  description = "Hash key for the Dynamodb Table"
+  type = string
+  default = "PK"
+}
+
+variable "sort_key" {
+  description = "Sort key for Dynamodb table"
+  type = string
+  default = "SK"
+}
+
+variable "environment" {
+    description = "Environment Name"
+    type = string
+}
+
+variable "enable_encription" {
+  description = "Enable server-side encription with aws KMS key"
+  type = bool
+  default = true
+}
+
+variable "kms_key_arn" {
+    description = "ARN of the KNM key for encription if not provided, ASW managed key will be used"
+    type = string
+    default = null
+}
+
+variable "point_in_time_recovery_enabled" {
+  description = "Enable point-in-time recovery for the table"
+  type = bool
+  default = true
+}
