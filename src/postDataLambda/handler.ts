065b76ed04c7a31c697c14ec17a1bef368ff8633
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2025-11-27 08:30:57 +0000

    chore: middy error hander

--- src/postDataLambda/handler.ts
+++ src/postDataLambda/handler.ts
@@ -2,7 +2,7 @@ import type { APIGatewayProxyEventV2 } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
   return {
-    status: 200,
+    statusCode: 200,
     body: 'Hello POST data',
   };
 };
