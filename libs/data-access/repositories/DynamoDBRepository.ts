cce480c7d56e2b7b9b4af6503625e60ff3427837
phillarmstrong-GDS <phill.armstrong@digital.cabinet-office.gov.uk>
2025-12-01 11:00:04 +0000

    feat(000): moving logger into utils so that it can be shared

--- libs/data-access/repositories/DynamoDBRepository.ts
+++ libs/data-access/repositories/DynamoDBRepository.ts
@@ -2,7 +2,7 @@ import { DynamoDBEntity } from '../types/Entity';
 import { Repository } from './Repository';
 import { DynamoDBDocumentClient, GetCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
 import { GetError, SaveError } from '../errors/Errors';
-import { logger } from '../../utils/Logger';
+import { logger } from '../../utils/Logger/Logger';
 
 
 /**
