fc1aadd071a146ef72a282dcfeab8d151ac3323d
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2026-01-23 10:15:45 +0000

    fix: Cognito naming clash

--- cdk/lib/constructs/cognito-construct.ts
+++ cdk/lib/constructs/cognito-construct.ts
@@ -35,8 +35,8 @@ export class CognitoConstruct extends Construct {
     const {
       developerId,
       environment,
-      userPoolName = 'api-auth',
-      domainPrefix = 'api-auth',
+      userPoolName = 'gov-udp-api-auth',
+      domainPrefix = 'gov-udp-api-auth',
       resourceServerIdentifier = 'udp',
       resourceServerName = 'API',
       resorceServerScopes = [
