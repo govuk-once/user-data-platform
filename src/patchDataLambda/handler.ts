49cf5eede860a549acaaa75ea693a03270b624ed
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2026-03-13 16:16:08 +0000

    fix: format

--- src/patchDataLambda/handler.ts
+++ src/patchDataLambda/handler.ts
@@ -15,7 +15,8 @@ import {
 } from '@libs/utils';
 import { ServiceFactory } from '@libs/data-access';
 
-const { STACK: stack, SERVICE_NAME: serviceName = 'udpPatchData' } = process.env;
+const { STACK: stack, SERVICE_NAME: serviceName = 'udpPatchData' } =
+  process.env;
 
 const tracer = getTracer({ serviceName });
 const logger = getLogger({
