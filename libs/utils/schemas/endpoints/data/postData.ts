8e0dc941f5496fd0edcf364b0a570d268398924c
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2026-04-22 15:26:07 +0100

    fix: dependancies/ timstamp open api issue

--- libs/utils/schemas/endpoints/data/postData.ts
+++ libs/utils/schemas/endpoints/data/postData.ts
@@ -8,7 +8,7 @@ extendZodWithOpenApi(z);
  Response Schemas & Types
 */
 
-const getCurrentTime = Math.floor(Date.now() / 1000);
+const getCurrentTime = 1776867570
 
 const dataObjectSchema = z
   .object()
