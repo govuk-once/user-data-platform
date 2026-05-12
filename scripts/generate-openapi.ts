7181bf9e028e4fbc1867d8e30738b3df4b09693b
CraigDeeming <craig.deeming@digital.cabinet-office.gov.uk>
2026-05-12 16:40:42 +0100

    test: test

--- scripts/generate-openapi.ts
+++ scripts/generate-openapi.ts
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 const OUTPUT_PATH = path.resolve(__dirname, '../docs/openapi.yml');
 const DOCS_DIR = path.dirname(OUTPUT_PATH);
 
+console.log('test');
+
 async function main(): Promise<number> {
   try {
     if (!fs.existsSync(DOCS_DIR)) {
