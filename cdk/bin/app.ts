eb1e0010422013407a5ff286ac1848e1ce3f0da7
Sandy Zhang <sandy.zhang@digital.cabinet-office.gov.uk>
2026-07-21 23:31:20 +0100

    feat: create dvla pilot stack

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -4,6 +4,7 @@ import {
   MainStack,
   MonitoringStack,
   SarStack,
+  DvlaPilotStack,
 } from 'cdk/lib/stacks';
 import { GovUkOnceEnvironments, repoMetaData } from '../constants/environment';
 import { VpcStack } from 'cdk/lib/stacks/vpc-stack';
@@ -121,6 +122,21 @@ if (!skipMainStack) {
   });
 
   sarStack.addDependency(mainStack);
+
+  const dvlaPilotStack = new DvlaPilotStack(app, `${stackPrefix}-dvla-pilot`, {
+    developerId,
+    environment,
+    stackPrefix,
+    env: awsEnv,
+    description: `DVLA pilot purge stack ${stackDescription}`,
+    identityTable: mainStack.identityTable,
+    kmsKey: mainStack.kmsKey,
+    dbKmsKey: mainStack.dbKmsKey,
+    vpc: vpcStack.vpc,
+    lambdaSecurityGroups: vpcStack.lambdaSecurityGroup,
+  });
+
+  dvlaPilotStack.addDependency(mainStack);
 
   // Hidden until Macie resource conflict can be resolved
   // if (macieStack) {
---
+++ cdk/lib/stacks/dvla-pilot-stack.ts
@@ -0,0 +1,67 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { ISecurityGroup, IVpc } from 'aws-cdk-lib/aws-ec2';
+import { IKey } from 'aws-cdk-lib/aws-kms';
+import { Construct } from 'constructs';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { LambdaApiConstruct } from '../constructs/lambda-construct';
+import { getLogRetentionPeriod } from 'cdk/constants/environment';
+
+export interface DvlaPilotStackProps extends StackProps {
+  developerId?: string;
+  environment: string;
+  stackPrefix: string;
+  identityTable: Table;
+  kmsKey: IKey;
+  dbKmsKey: IKey;
+  vpc?: IVpc;
+  lambdaSecurityGroups?: ISecurityGroup;
+}
+
+export class DvlaPilotStack extends Stack {
+  constructor(scope: Construct, id: string, props: DvlaPilotStackProps) {
+    super(scope, id, props);
+
+    const {
+      developerId,
+      environment,
+      stackPrefix,
+      identityTable,
+      kmsKey,
+      dbKmsKey,
+      vpc,
+      lambdaSecurityGroups,
+    } = props;
+
+    const purgeKeySecret = Secret.fromSecretNameV2(
+      this,
+      'DvlaPilotPurgeKeySecret',
+      '/identity/dvla/pilot/purge-key',
+    );
+
+    const dvlaPilotPurgeLambda = new LambdaApiConstruct(
+      this,
+      'dvlaPilotPurge',
+      {
+        developerId,
+        environment,
+        functionName: 'dvla-pilot-purge',
+        sourcePath: 'dvlaPilotPurgeLambda',
+        kmsKey,
+        dbKmsKey,
+        identityDbTable: identityTable,
+        identityDbActions: ['dynamodb:Scan', 'dynamodb:DeleteItem'],
+        environmentVariables: {
+          STACK: stackPrefix,
+          SERVICE_NAME: 'dvlaPilotPurge',
+          PURGE_KEY_SECRET_NAME: purgeKeySecret.secretName,
+        },
+        vpc,
+        securityGroups: lambdaSecurityGroups ? [lambdaSecurityGroups] : [],
+        logRetentionDays: getLogRetentionPeriod(environment),
+      },
+    );
+
+    purgeKeySecret.grantRead(dvlaPilotPurgeLambda.function);
+  }
+}
