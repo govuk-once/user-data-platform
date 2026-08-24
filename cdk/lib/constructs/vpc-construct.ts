1a4576b5fea58ad3a6d22de500424ea3b9302cc4
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2026-08-24 15:22:05 +0100

    fix: conditional nacl

--- cdk/lib/constructs/vpc-construct.ts
+++ cdk/lib/constructs/vpc-construct.ts
@@ -8,6 +8,7 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import {
   getLogRetentionPeriod,
   getRemovalPolicy,
+  GovUkOnceEnvironments,
 } from 'cdk/constants/environment';
 
 export interface VpcConstructProps {
@@ -448,21 +449,32 @@ export class VpcConstruct extends Construct {
       });
     }
 
-    this.publicNacl.addEntry('AllowAllInbound', {
-      ruleNumber: 100,
-      cidr: ec2.AclCidr.anyIpv4(),
-      traffic: ec2.AclTraffic.allTraffic(),
-      direction: ec2.TrafficDirection.INGRESS,
-      ruleAction: ec2.Action.ALLOW,
-    });
+    // A Custom NACL to allow performance tests to run on dev and stage
+    if (this.environment !== GovUkOnceEnvironments.Prod) {
+      this.publicNacl.addEntry('AllowInboundFromVpc', {
+        ruleNumber: 100,
+        cidr: ec2.AclCidr.ipv4(this.vpcCidr),
+        traffic: ec2.AclTraffic.allTraffic(),
+        direction: ec2.TrafficDirection.INGRESS,
+        ruleAction: ec2.Action.ALLOW,
+      });
 
-    this.publicNacl.addEntry('AllowAllOutbound', {
-      ruleNumber: 100,
-      cidr: ec2.AclCidr.anyIpv4(),
-      traffic: ec2.AclTraffic.allTraffic(),
-      direction: ec2.TrafficDirection.EGRESS,
-      ruleAction: ec2.Action.ALLOW,
-    });
+      this.publicNacl.addEntry('AllowInboundEphemeral', {
+        ruleNumber: 110,
+        cidr: ec2.AclCidr.anyIpv4(),
+        traffic: ec2.AclTraffic.tcpPortRange(1024, 65535),
+        direction: ec2.TrafficDirection.INGRESS,
+        ruleAction: ec2.Action.ALLOW,
+      });
+
+      this.publicNacl.addEntry('AllowAllOutbound', {
+        ruleNumber: 100,
+        cidr: ec2.AclCidr.anyIpv4(),
+        traffic: ec2.AclTraffic.allTraffic(),
+        direction: ec2.TrafficDirection.EGRESS,
+        ruleAction: ec2.Action.ALLOW,
+      });
+    }
 
     this.vpc.publicSubnets.forEach((subnet, index) => {
       new ec2.SubnetNetworkAclAssociation(
