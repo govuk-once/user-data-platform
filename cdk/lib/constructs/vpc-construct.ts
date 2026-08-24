3f0274ce88e8c523d34f2220e148de3dc08b8448
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2026-08-24 15:38:41 +0100

    fix: remove

--- cdk/lib/constructs/vpc-construct.ts
+++ cdk/lib/constructs/vpc-construct.ts
@@ -450,31 +450,31 @@ export class VpcConstruct extends Construct {
     }
 
     // A Custom NACL to allow performance tests to run on dev and stage
-    if (this.environment !== GovUkOnceEnvironments.Prod) {
-      this.publicNacl.addEntry('AllowInboundFromVpc', {
-        ruleNumber: 100,
-        cidr: ec2.AclCidr.ipv4(this.vpcCidr),
-        traffic: ec2.AclTraffic.allTraffic(),
-        direction: ec2.TrafficDirection.INGRESS,
-        ruleAction: ec2.Action.ALLOW,
-      });
+    // if (this.environment !== GovUkOnceEnvironments.Prod) {
+    //   this.publicNacl.addEntry('AllowInboundFromVpc', {
+    //     ruleNumber: 100,
+    //     cidr: ec2.AclCidr.ipv4(this.vpcCidr),
+    //     traffic: ec2.AclTraffic.allTraffic(),
+    //     direction: ec2.TrafficDirection.INGRESS,
+    //     ruleAction: ec2.Action.ALLOW,
+    //   });
 
-      this.publicNacl.addEntry('AllowInboundEphemeral', {
-        ruleNumber: 110,
-        cidr: ec2.AclCidr.anyIpv4(),
-        traffic: ec2.AclTraffic.tcpPortRange(1024, 65535),
-        direction: ec2.TrafficDirection.INGRESS,
-        ruleAction: ec2.Action.ALLOW,
-      });
+    //   this.publicNacl.addEntry('AllowInboundEphemeral', {
+    //     ruleNumber: 110,
+    //     cidr: ec2.AclCidr.anyIpv4(),
+    //     traffic: ec2.AclTraffic.tcpPortRange(1024, 65535),
+    //     direction: ec2.TrafficDirection.INGRESS,
+    //     ruleAction: ec2.Action.ALLOW,
+    //   });
 
-      this.publicNacl.addEntry('AllowAllOutbound', {
-        ruleNumber: 100,
-        cidr: ec2.AclCidr.anyIpv4(),
-        traffic: ec2.AclTraffic.allTraffic(),
-        direction: ec2.TrafficDirection.EGRESS,
-        ruleAction: ec2.Action.ALLOW,
-      });
-    }
+    //   this.publicNacl.addEntry('AllowAllOutbound', {
+    //     ruleNumber: 100,
+    //     cidr: ec2.AclCidr.anyIpv4(),
+    //     traffic: ec2.AclTraffic.allTraffic(),
+    //     direction: ec2.TrafficDirection.EGRESS,
+    //     ruleAction: ec2.Action.ALLOW,
+    //   });
+    // }
 
     this.vpc.publicSubnets.forEach((subnet, index) => {
       new ec2.SubnetNetworkAclAssociation(
